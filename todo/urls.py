bfc6ca457a7358e3158a866ac78fb87a89a8c36d
Akarsh Reddy Eathamukkalaclear <akarsh16reddy@gmail.com>
2024-10-30 20:46:55 -0400

    resolves #8: adding template deletion

--- todo/urls.py
+++ todo/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
-    path("password_reset", views.password_reset_request, name="password_reset")
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('templates/delete/<int:template_id>', views.delete_template, name='delete_template')
 ]
